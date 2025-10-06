9f282cbecdf725a18a539454045447f248582d06
GrinDi <ferznii5555@gmail.com>
2025-10-07 01:44:52 +0300

    Конечная дороботка

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -23,6 +23,6 @@ def show_catalog(request):
 
 def show_product(request, slug):
     template = 'product.html'
-    phone = Phone.objects.filter(slug__contains=slug)
+    phone = Phone.objects.get(slug=slug)
     context = {'phone': phone}
     return render(request, template, context)
