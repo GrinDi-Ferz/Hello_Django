892731d1a502c33bfa337ac5bb8436fa4a930ace
GrinDi <Grin1990Di@gmail.com>
2025-12-17 00:20:00 +0300

    Commit 4

--- 3.2-crud/stocks_products/stocks_products/urls.py
+++ 3.2-crud/stocks_products/stocks_products/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.http import HttpResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('logistic.urls')),
+    path('', lambda request: HttpResponse("Главная страница")),
 ]
